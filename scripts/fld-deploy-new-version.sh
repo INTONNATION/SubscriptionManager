b06291dbba14c347e4094f8a66648f1e9301035f
Anton Zavodovskiy <azavodovskiy@pixsystem.com>
2021-12-24 15:58:57 +0200

    new versions

--- scripts/fld-deploy-new-version.sh
+++ scripts/fld-deploy-new-version.sh
@@ -7,6 +7,15 @@ subscriptionIndexTvc=$(cat ../abi/SubscriptionIndex.tvc | base64)
 serviceTvc=$(cat ../abi/SubscriptionService.tvc | base64)
 serviceIndexTvc=$(cat ../abi/SubscriptionServiceIndex.tvc | base64)
 walletTvc=$(cat ../abi/Wallet.tvc | base64)
+# abiServiceContract=$(cat ../abi/SubscriptionService.abi.json | jq -c .| base64)
+# abiServiceIndexContract=$(cat ../abi/SubscriptionServiceIndex.abi.json | jq -c .| base64)
+# abiSubscriptionIndexContract=$(cat ../abi/SubscriptionIndex.abi.json | jq -c .| base64)
+# abiSubsManDebot=$(cat ../abi/SubsMan.abi.json | jq -c .| base64)
+abiServiceContract=$(cat ../abi/ServiceContract.json | jq -c .| base64)
+abiServiceIndexContract=$(cat ../abi/ServiceIndexContract.json | jq -c .| base64)
+abiSubscriptionIndexContract=$(cat ../abi/SubscriptionIndexContract.json | jq -c .| base64)
+abiSubsManDebot=$(cat ../abi/SubsManDebot.json | jq -c .| base64)
+
 
 LOCALNET=http://127.0.0.1
 DEVNET=https://net.ton.dev
@@ -21,4 +30,6 @@ echo $configAddr
 
 
 tonos-cli --url $NETWORK call $configAddr setTvc "{\"tvcServiceInput\":\"$serviceTvc\", \"tvcWalletInput\":\"$walletTvc\",\"tvcSubsciptionInput\":\"$subscriptionTvc\",\"tvcSubscriptionServiceIndexInput\":\"$serviceIndexTvc\",\"tvcSuscriptionIndexInput\":\"$subscriptionIndexTvc\"}"  --abi ../abi/configVersions.abi.json --sign configVersions.keys.json
+tonos-cli --url $NETWORK call $configAddr setAbi "{\"abiServiceContractInput\":\"$abiServiceContract\",\"abiServiceIndexContractInput\":\"$abiServiceIndexContract\",\"abiSubscriptionIndexContractInput\":\"$abiSubscriptionIndexContract\",\"abiSubsManDebotInput\":\"$abiSubsManDebot\"}"  --abi ../abi/configVersions.abi.json --sign configVersions.keys.json
+
 
