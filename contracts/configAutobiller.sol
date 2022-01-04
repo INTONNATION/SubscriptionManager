08a8485e8718247762897543191cd710f87b17bf
Renat Skitsan <renatskitsan@gmail.com>
2022-01-04 04:32:02 -0800

    Update style

--- contracts/configAutobiller.sol
+++ contracts/configAutobiller.sol
@@ -2,11 +2,10 @@ pragma ton-solidity ^ 0.51.0;
 pragma AbiHeader expire;
 pragma AbiHeader time;
 
+
 contract configAutobiller {
-  
 
     uint8 public versionAutobiller;
-        
 	struct VersionsAutobiller {
 		string pubkey;
         string name;
@@ -17,39 +16,47 @@ contract configAutobiller {
         string to;
         string category;
 	}
-
 	mapping (uint8 => VersionsAutobiller) public vrsAutobiller;
 
 	constructor() public {
-		require(tvm.pubkey() != 0, 101);
+		require(msg.pubkey() == tvm.pubkey(), 100);
 		tvm.accept();
 	}
 
-    
     modifier onlyOwner {
 		require(msg.pubkey() == tvm.pubkey(), 100);
 		tvm.accept();
 		_;
     }
-    function getAutobillerLatest() public view returns(optional (VersionsAutobiller)){
+
+    function getAutobillerLatest() public view returns(optional(VersionsAutobiller)) {
         optional(VersionsAutobiller) value = vrsAutobiller.fetch(versionAutobiller);
 		return value;
     }
 
-    function getAutobillerVersions() public view returns (uint8[] arr ){
+    function getAutobillerVersions() public view returns (uint8[] arr) {
         for ((uint8 k,) : vrsAutobiller) {
-        arr.push(k);
+        	arr.push(k);
         }
     }   
 
-	
 	// get configs by version
-	function getAutobillerVersion(uint8 versionAutobiller) public view returns(optional (VersionsAutobiller)){
+	function getAutobillerVersion(uint8 versionAutobiller) public view returns(optional(VersionsAutobiller)) {
         optional(VersionsAutobiller) value = vrsAutobiller.fetch(versionAutobiller);
 		return value;
     }
 
-    function setAutobillerConfig(string pubkeyINPUT, string nameINPUT, string periodINPUT, string valueINPUT, string imageINPUT, string descriptionINPUT, string toINPUT, string categoryINPUT)  public onlyOwner {
+    function setAutobillerConfig(
+		string pubkeyINPUT, 
+		string nameINPUT, 
+		string periodINPUT, 
+		string valueINPUT, 
+		string imageINPUT, 
+		string descriptionINPUT, 
+		string toINPUT, 
+		string categoryINPUT
+	) public onlyOwner 
+	{
 		versionAutobiller++;
 		VersionsAutobiller params;
 		params.pubkey = pubkeyINPUT;
@@ -62,6 +69,4 @@ contract configAutobiller {
 		params.category = categoryINPUT;
 		vrsAutobiller.add(versionAutobiller, params);
     }
-
 }
-
--- contracts/configConvert.sol
+++ contracts/configConvert.sol
@@ -2,30 +2,29 @@ pragma ton-solidity ^ 0.51.0;
 pragma AbiHeader expire;
 pragma AbiHeader time;
 
+
 contract configVersions {
         
 	TvmCell public tvcWallet;
-
 	address public mRootAddr;
 	address public RootAddr;
 	address public mConvertWalletAddr;
 	address public ConvertWalletAddr;
-
 	string public mRootTokenContract;
 	string public mTONTokenWalletContract;
 	string public RootTokenContract;
 	string public TONTokenWalletContract;
-
-	constructor() public {
-		require(tvm.pubkey() != 0, 101);
-		tvm.accept();
-	}
 
     modifier onlyOwner {
 		require(msg.pubkey() == tvm.pubkey(), 100);
 		tvm.accept();
 		_;
     }
+
+	constructor() public {
+		require(tvm.pubkey() != 0, 101);
+		tvm.accept();
+	}
 
 	function setTvcWallet(TvmCell tvcWalletINPUT) public onlyOwner {
 		tvcWallet = tvcWalletINPUT;
@@ -38,6 +37,7 @@ contract configVersions {
 	function setRootAddr(address RootAddrINPUT) public onlyOwner {
 		RootAddr = RootAddrINPUT;
 	}
+
 	function setmConvertWalletAddr(address mConvertWalletAddrINPUT) public onlyOwner {
 		mConvertWalletAddr = mConvertWalletAddrINPUT;
 	}
@@ -61,6 +61,4 @@ contract configVersions {
 	function setAbiTONTokenWalletContract(string TONTokenWalletContractINPUT) public onlyOwner {
 		TONTokenWalletContract = TONTokenWalletContractINPUT;
 	}
-
 }
-
