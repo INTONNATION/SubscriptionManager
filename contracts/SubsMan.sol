69d0b4a0a899227cbbeb859f9cb8b49b965596d6
sostrovskyi <sostrovskyi>
2022-01-15 22:52:18 +0200

    WIP

--- contracts/SubsMan.sol
+++ contracts/SubsMan.sol
@@ -145,7 +145,7 @@ contract SubsMan is Upgradable {
     }
  
     function deployService(TvmCell params, string serviceCategory) public view {
-        require(msg.value >= 1 ton, SubsManErrors.error_not_enough_balance_in_message);
+        require(msg.value >= 0.05 ton, SubsManErrors.error_not_enough_balance_in_message);
         require(msg.sender != address(0), SubsManErrors.error_message_sender_address_not_specified);
         TvmCell serviceStateInit = buildService(
             msg.sender,
