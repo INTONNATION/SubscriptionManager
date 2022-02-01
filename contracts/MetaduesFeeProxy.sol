8a3ca9a935c8e81c4f3aa9dc982789e800ae1cb7
Anton Zavodovskiy <azavodovskiy@pixsystem.com>
2022-02-01 18:43:12 +0200

    add fee proxy

---
+++ contracts/MetaduesFeeProxy.sol
@@ -0,0 +1,25 @@
+pragma ton-solidity ^ 0.56.0;
+pragma AbiHeader expire;
+pragma AbiHeader time;
+pragma AbiHeader pubkey;
+
+
+import "libraries/MetaduesRootErrors.sol";
+import "./Platform.sol";
+import "libraries/PlatformTypes.sol";
+import "TIP3/interfaces/ITokenWallet.sol";
+
+
+contract MetaduesFeeProxy {
+   
+    mapping(address => uint128) balance_map;
+    address public root;
+    TvmCell platform_code;
+    TvmCell platform_params;
+    uint32 current_version;
+    uint8 type_id;
+
+    constructor() public { revert(); }
+    
+    function fee(TvmCell params) public returns (TvmCell){
+}
