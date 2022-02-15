bf3bbf264e4c3cc8df306da52da8dc66f8c8d1ec
sostrovskyi <sostrovskyi>
2022-02-15 09:42:35 +0200

    WIP

--- contracts/libraries/SubscriptionErrors.sol
+++ contracts/libraries/SubscriptionErrors.sol
@@ -8,7 +8,7 @@ library SubscriptionErrors {
     uint8 constant error_define_wallet_address_in_static_vars    = 104;
     uint8 constant error_not_enough_balance_in_message           = 105;
     uint8 constant error_incorrect_service_params                = 106;
-    uint8 constant error_message_sender_is_not_index             = 107;
+    uint8 constant error_message_sender_is_not_owner            = 107;
     uint8 constant error_subscription_status_already_active      = 108;
     uint8 constant incorrect_subscription_address_in_constructor = 109;
     uint8 constant error_message_sender_is_not_my_owner          = 110;
