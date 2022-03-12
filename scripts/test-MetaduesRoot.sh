64c37febfc4c393997d31ce26d887a9b13af06d6
Renat Skitsan <renatskitsan@gmail.com>
2022-03-12 04:16:12 -0800

    Add subscr

--- scripts/test-MetaduesRoot.sh
+++ scripts/test-MetaduesRoot.sh
@@ -44,20 +44,18 @@ $tos genaddr ../abi/$1.tvc ../abi/$1.abi.json > log.log
 
 
 METADUES_ROOT_ADDRESS=$(cat $CONTRACT_NAME.addr)
-MTDS_ROOT="0:abce5418c119d5ae797445b7b8951888570b84eb3071db526ff28166106af8ac"
 USDT_ROOT="0:c5f5a0f97da30c303808acac50eff22d81f4b268745cb43f352495711fe052b1"
 
 ##Deploy account
 $tos --url $NETWORK  call $wallet  submitTransaction "{\"dest\":\"$METADUES_ROOT_ADDRESS\",\"value\":200000000,\"bounce\":false,\"allBalance\":false,\"payload\":\"te6ccgEBAQEABgAACAOxbTU=\"}" --abi ../../ton-labs-contracts/solidity/safemultisig/SafeMultisigWallet.abi.json --sign test.msig.keys.json
-genaddr MetaduesAccount
-account_address=$(get_address)
+account_address=$(tonos-cli -j run $METADUES_ROOT_ADDRESS accountOf "{\"owner_address_\":\"$wallet\"}" --abi ../abi/MetaduesRoot.abi.json | jq -r .account)
 
 ##top up account wallet
 account_wallet=$($tos --url $NETWORK run $USDT_ROOT  walletOf "{\"answerId\":\"0\",\"walletOwner\":\"$account_address\"}" --abi ../../ton-eth-bridge-token-contracts/build/TokenRoot.abi.json|grep value0|awk '{print $2}' | tr -d \")
 
 owner_wallet=$($tos --url $NETWORK run $USDT_ROOT  walletOf "{\"answerId\":\"0\",\"walletOwner\":\"$wallet\"}" --abi ../../ton-eth-bridge-token-contracts/build/TokenRoot.abi.json | grep value0 | awk '{print $2}')
 
-payload=$(tonos-cli  --url https://gql.custler.net  body transferToWallet "{\"amount\": \"10\", \"recipientTokenWallet\": \"$account_wallet\", \"remainingGasTo\": \"$wallet\",\"notify\": \"true\",\"payload\": \"te6ccgEBAQEAAgAAAA==\"}"  --abi ../../ton-eth-bridge-token-contracts/build/TokenWallet.abi.json | grep 'Message body:' | awk '{print $3}')
+payload=$(tonos-cli --url $NETWORK body transferToWallet "{\"amount\": \"10\", \"recipientTokenWallet\": \"$account_wallet\", \"remainingGasTo\": \"$wallet\",\"notify\": \"true\",\"payload\": \"te6ccgEBAQEAAgAAAA==\"}"  --abi ../../ton-eth-bridge-token-contracts/build/TokenWallet.abi.json | grep 'Message body:' | awk '{print $3}')
 
 $tos --url $NETWORK  call $wallet  submitTransaction "{\"dest\":$owner_wallet,\"value\":200000000,\"bounce\":false,\"allBalance\":false,\"payload\":\"$payload\"}" --abi ../../ton-labs-contracts/solidity/safemultisig/SafeMultisigWallet.abi.json --sign test.msig.keys.json
 
@@ -67,14 +65,13 @@ sleep 10
 $tos --url $NETWORK run $account_address  wallets_mapping "{}" --abi ../abi/MetaduesAccount.abi.json
 
 ##Deploy service
-tonos-cli  --url https://gql.custler.net  call $METADUES_ROOT_ADDRESS  deployService "{\"service_params\":\"te6ccgEBBwEAtwABa4AbmUiTj4jAIB7gbWilvUp9Mdxf9W0+LFUhqoCKyU3O0eAAAAAAAAAAAAAAAAAgbMgAAAAD0AEEAAYFBAIBQ4AaqpoiehC9jd11Xg0kTEEvFKWi0oimoYdhTRY13+72U7ADAAhEZUZpAFxRbVl1NVYzcXRmejNtRENRQVd3WkN3Q21lY014Mm5wZm83Yk5odEZSTnVVTFlXABZsaWItdGVzdDJ2YQAcdGlwMy1saWItdGVzdDU=\"}" --abi ../abi/MetaduesRoot.abi.json
-genaddr SubscriptionService
-service_adress=$(get_address)
+tonos-cli --url $NETWORK call $METADUES_ROOT_ADDRESS  deployService "{\"service_params\":\"te6ccgEBBwEAtwABa4AbmUiTj4jAIB7gbWilvUp9Mdxf9W0+LFUhqoCKyU3O0eAAAAAAAAAAAAAAAAAgbMgAAAAD0AEEAAYFBAIBQ4AaqpoiehC9jd11Xg0kTEEvFKWi0oimoYdhTRY13+72U7ADAAhEZUZpAFxRbVl1NVYzcXRmejNtRENRQVd3WkN3Q21lY014Mm5wZm83Yk5odEZSTnVVTFlXABZsaWItdGVzdDJ2YQAcdGlwMy1saWItdGVzdDU=\"}" --abi ../abi/MetaduesRoot.abi.json
+service_adress=$(tonos-cli -j run $METADUES_ROOT_ADDRESS accountOf "{\"owner_address_\":\"$wallet\",\"service_name_\":\"lib-test\"}" --abi ../abi/MetaduesRoot.abi.json | jq -r .account)
 
 
 ##Deploy Subscription
-tonos-cli  --url https://gql.custler.net  body deploySubscription "{\"service_address\": \"$service_adress\", \"identificator\": \"te6ccgEBAQEAAgAAAA==\"}" --abi ../abi/MetaduesRoot.abi.json
+tonos-cli --url $NETWORK body deploySubscription "{\"service_address\": \"$service_adress\", \"identificator\": \"te6ccgEBAQEAAgAAAA==\"}" --abi ../abi/MetaduesRoot.abi.json
 
 ##Check account balance #2
 sleep 120
-$tos --url $NETWORK run $account_adress  wallets_mapping "{}" --abi ../abi/MetaduesAccount.abi.json
+$tos --url $NETWORK run $account_address  wallets_mapping "{}" --abi ../abi/MetaduesAccount.abi.json
