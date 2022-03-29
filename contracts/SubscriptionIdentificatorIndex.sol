70aa53242698832e8adc46fd46e7aeb832ccd225
Renat Skitsan <renatskitsan@gmail.com>
2022-03-29 01:33:17 -0700

    Update and bug fixes

--- contracts/SubscriptionIdentificatorIndex.sol
+++ contracts/SubscriptionIdentificatorIndex.sol
@@ -1,4 +1,4 @@
-pragma ton-solidity >= 0.56.0;
+pragma ton-solidity >=0.56.0;
 pragma AbiHeader expire;
 pragma AbiHeader time;
 pragma AbiHeader pubkey;
@@ -6,22 +6,29 @@ pragma AbiHeader pubkey;
 import "libraries/SubscriptionErrors.sol";
 
 contract SubscriptionIdentificatorIndex {
-    
     address static subscription_owner;
     address public subscription_address;
     address public root;
     TvmCell public identificator;
 
-
-    modifier onlyOwner {
-		require(msg.sender == subscription_address, SubscriptionErrors.error_message_sender_is_not_owner);
-		_;
+    modifier onlyOwner() {
+        require(
+            msg.sender == subscription_address,
+            SubscriptionErrors.error_message_sender_is_not_owner
+        );
+        _;
     }
 
     constructor(address subsAddr) public {
         optional(TvmCell) salt = tvm.codeSalt(tvm.code());
-        (TvmCell identificator_, address root_) = salt.get().toSlice().decode(TvmCell, address);
-        require(msg.sender == root_, SubscriptionErrors.error_message_sender_is_not_root);
+        (TvmCell identificator_, address root_) = salt.get().toSlice().decode(
+            TvmCell,
+            address
+        );
+        require(
+            msg.sender == root_,
+            SubscriptionErrors.error_message_sender_is_not_root
+        );
         root = root_;
         identificator = identificator_;
         subscription_address = subsAddr;
@@ -36,8 +43,7 @@ contract SubscriptionIdentificatorIndex {
         onCodeUpgrade(builder.toCell());
     }
 
-    function onCodeUpgrade(TvmCell upgrade_data) private {
-    }
+    function onCodeUpgrade(TvmCell upgrade_data) private {}
 
     function cancel() public onlyOwner {
         selfdestruct(subscription_owner);
