d51b9bbee11a1d4f9f52692b2aa4733a30d930f3
Renat Gofman <renatgofman@gmail.com>
2022-04-26 17:26:50 +0300

    WIP

---
+++ contracts/libraries/EverduesSubscriptionStatus.sol
@@ -0,0 +1,7 @@
+pragma ton-solidity >=0.56.0;
+
+library EverduesSubscriptionStatus {
+	uint8 constant STATUS_ACTIVE = 1;
+	uint8 constant STATUS_NONACTIVE = 2;
+	uint8 constant STATUS_PROCESSING = 3;
+}
