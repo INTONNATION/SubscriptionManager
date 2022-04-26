408852207da6c1684ce57f4e40516bd602e67ccf
Renat Gofman <renatgofman@gmail.com>
2022-04-26 17:26:50 +0300

    WIP

---
+++ contracts/interfaces/IEverduesSubscription.sol
@@ -0,0 +1,7 @@
+pragma ton-solidity >=0.56.0;
+
+interface IEverduesSubscription {
+    event paramsRecieved(TvmCell service_params_);
+    function subscriptionStatus() external returns (uint8);
+    function executeSubscription(uint128 paySubscriptionGas) external;
+}
