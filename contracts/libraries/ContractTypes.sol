38bd13793818957dbb822ad264cd8f30a39019bd
Renat Gofman <renatgofman@gmail.com>
2022-07-11 23:51:29 +0300

    fixes and improvments

---
+++ contracts/libraries/ContractTypes.sol
@@ -0,0 +1,12 @@
+pragma ton-solidity >=0.56.0;
+
+library ContractTypes {
+	uint8 constant Account = 1;
+	uint8 constant Service = 2;
+	uint8 constant Subscription = 3;
+	uint8 constant FeeProxy = 4;
+	uint8 constant SubscriptionIdentificatorIndex = 5;
+	uint8 constant SubscriptionIndex = 6;
+	uint8 constant ServiceIndex = 7;
+	uint8 constant ServiceIdentificatorIndex = 8;
+}
