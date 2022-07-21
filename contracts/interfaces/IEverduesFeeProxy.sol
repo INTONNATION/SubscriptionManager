8079521301a9f659698842096543a6e1f0d744cf
Renat Gofman <renatgofman@gmail.com>
2022-07-21 23:21:33 +0300

    project structure reogranization

--- contracts/interfaces/IEverduesFeeProxy.sol
+++ contracts/interfaces/IEverduesFeeProxy.sol
@@ -1,7 +1,6 @@
 pragma ton-solidity >=0.56.0;
 
 interface IEverduesFeeProxy {
-
 	function executePaySubscription(
 		address account_address,
 		address service_address,
@@ -12,8 +11,32 @@ interface IEverduesFeeProxy {
 		uint128 additional_gas
 	) external;
 
-	function setAccountGasThreshold(uint128 account_threshold_,	address send_gas_to) external;
-	function setSupportedCurrencies(TvmCell fee_proxy_contract_params,address send_gas_to) external;
-	function setMTDSRootAddress(address mtds_root, address send_gas_to) external;
+	function setAccountGasThreshold(
+		uint128 account_threshold_,
+		address send_gas_to
+	) external;
+
+	function setSupportedCurrencies(
+		TvmCell fee_proxy_contract_params,
+		address send_gas_to
+	) external;
+
+	function setMTDSRootAddress(address mtds_root, address send_gas_to)
+		external;
+
 	function setDexRootAddress(address dex_root, address send_gas_to) external;
+
+	function transferRevenue(address revenue_to, address send_gas_to) external;
+
+	function swapRevenueToMTDS(address currency_root, address send_gas_to)
+		external;
+
+	function syncBalance(address currency_root, address send_gas_to) external;
+
+	function upgrade(
+		TvmCell code,
+		uint32 version,
+		address send_gas_to,
+		TvmCell contract_params
+	) external;
 }
