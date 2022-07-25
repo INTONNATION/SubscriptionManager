55c8e0612391c12e0fedad08dd1c77d0203897a1
Renat Gofman <renatgofman@gmail.com>
2022-07-25 19:27:40 +0300

    fix index cancel

--- everdues/interfaces/IEverduesIndex.sol
+++ everdues/interfaces/IEverduesIndex.sol
@@ -1,7 +1,7 @@
 pragma ton-solidity >=0.56.0;
 
 interface IEverduesIndex {
-	function cancel() external;
+	function cancel(address send_gas_to) external;
 
 	function updateIndexData(TvmCell index_data, address send_gas_to) external;
 }
