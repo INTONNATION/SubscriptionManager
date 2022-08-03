fa90296756d72009fcf032255de5ed5f9a226f10
aZ! <az@aZs-MacBook-Pro.local>
2022-08-03 11:40:17 +0300

    rename tondev to the new name

--- everdues/scripts/compile.sh
+++ everdues/scripts/compile.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 set -xe
 
-tondev sol compile ../contracts/root/EverduesRoot.sol -o ../abi;
-tondev sol compile ../contracts/account/EverduesAccountV1.sol -o ../abi;
-tondev sol compile ../contracts/Platform.sol -o ../abi;
-tondev sol compile ../contracts/subscription/EverduesSubscriptionV1.sol -o ../abi;
-tondev sol compile ../contracts/Index.sol -o ../abi;
-tondev sol compile ../contracts/service/EverduesServiceV1.sol -o ../abi;
-tondev sol compile ../contracts/feeProxy/EverduesFeeProxy.sol -o ../abi;
+everdev sol compile ../contracts/root/EverduesRoot.sol -o ../abi;
+everdev sol compile ../contracts/account/EverduesAccountV1.sol -o ../abi;
+everdev sol compile ../contracts/Platform.sol -o ../abi;
+everdev sol compile ../contracts/subscription/EverduesSubscriptionV1.sol -o ../abi;
+everdev sol compile ../contracts/Index.sol -o ../abi;
+everdev sol compile ../contracts/service/EverduesServiceV1.sol -o ../abi;
+everdev sol compile ../contracts/feeProxy/EverduesFeeProxy.sol -o ../abi;
