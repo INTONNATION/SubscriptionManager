58912a863d3b30b1705d52afeadee763d39ed8c8
Renat Gofman <renatgofman@gmail.com>
2022-08-19 13:43:13 +0300

    add subscriptionExecuted event

--- everdues/interfaces/IEverduesSubscription.sol
+++ everdues/interfaces/IEverduesSubscription.sol
@@ -3,7 +3,8 @@ pragma ton-solidity >=0.56.0;
 interface IEverduesSubscription {
 	event paramsRecieved(TvmCell service_params_);
 	event SubscriptionDeleted();
-
+	event subscriptionExecuted();
+	
 	function subscriptionStatus() external returns (uint8);
 
 	function executeSubscription(uint128 paySubscriptionGas) external;
