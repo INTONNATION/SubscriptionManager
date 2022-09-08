423f0b98a6b6b5fd85e9fff29771466e6721cc3a
Renat Gofman <renatgofman@gmail.com>
2022-09-08 09:26:15 -0700

    adjust gas

--- everdues/libraries/EverduesGas.sol
+++ everdues/libraries/EverduesGas.sol
@@ -2,28 +2,23 @@ pragma ton-solidity >=0.56.0;
 
 library EverduesGas {
 
-    uint128 constant ROOT_INITIAL_BALANCE                   = 1 ever;
-    uint128 constant FEE_PROXY_INITIAL_BALANCE              = 1 ever;
     uint128 constant UPGRADE_MIN_VALUE                      = 1 ever;
 
+    uint128 constant ROOT_INITIAL_BALANCE                   = 1 ever;
+    uint128 constant FEE_PROXY_INITIAL_BALANCE              = 1 ever;
+
     uint128 constant ACCOUNT_INITIAL_BALANCE                = 0.5 ever;
-
     uint128 constant SUBSCRIPTION_INITIAL_BALANCE           = 0.5 ever;
-    uint128 constant EXECUTE_SUBSCRIPTION_VALUE             = 0.2 ever;
+    uint128 constant SERVICE_INITIAL_BALANCE                = 0.5 ever;
+    uint128 constant INDEX_INITIAL_BALANCE                  = 0.1 ever;
+
     uint128 constant DEPLOY_SUBSCRIPTION_VALUE              = 0.5 ever;
-
-
-    uint128 constant DEPLOY_SERVICE_VALUE_ACCOUNT           =  2 ever;
-    uint128 constant DEPLOY_SERVICE_VALUE_ROOT              =  1 ever;
-    uint128 constant SERVICE_INITIAL_BALANCE                =  1 ever;
-    
-    uint128 constant INDEX_INITIAL_BALANCE                  = 0.1 ever; // TODO calculate storage fee and reduce
-
-    uint128 constant TRANSFER_MIN_VALUE                     = 0.4 ever;
-    uint128 constant MESSAGE_MIN_VALUE                      = 0.2 ever;
-
+    uint128 constant DEPLOY_SERVICE_VALUE                   = 1.5 ever;
     uint128 constant DEPLOY_EMPTY_WALLET_VALUE              = 0.5 ever;
     uint128 constant DEPLOY_EMPTY_WALLET_GRAMS              = 0.1 ever;
 
+    uint128 constant EXECUTE_SUBSCRIPTION_VALUE             = 0.2 ever;
+    uint128 constant TRANSFER_MIN_VALUE                     = 0.4 ever;
+    uint128 constant MESSAGE_MIN_VALUE                      = 0.2 ever;
     uint128 constant SWAP_TIP3_TO_EVER_MIN_VALUE            = 4 ever;
 }
