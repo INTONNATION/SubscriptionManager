e432ab33691c7812d06edccf2a922666d8646733
Renat Gofman <renatgofman@gmail.com>
2022-10-14 11:53:39 +0300

    Change deploy scripts to support auto-upgrade

--- everdues/scripts/upgrade-everdues-root.sh
+++ everdues/scripts/upgrade-everdues-root.sh
@@ -17,7 +17,7 @@ CONTRACT_NAME=EverduesRoot
 
 function deploy {
 CONTRACT_ADDRESS=`cat ./envs/$2-EverduesRoot.addr`
-owner=`cat dev-single.msig.addr| grep "Raw address" | awk '{print $3}'`
+owner=`cat dev-single.msig.addr`
 code=`tvm_linker decode --tvc ../abi/EverduesRoot.tvc | grep code: | awk '{ print $2 }'`
 message=`tonos-cli -j body upgrade "{\"code\":\"$code\"}"  --abi ../abi/$1.abi.json | jq -r .Message`
 tonos-cli callx -m submitTransaction --addr $owner --abi ../abi/SafeMultisigWallet.abi.json --keys owner.msig.keys.json --dest $CONTRACT_ADDRESS --value 5T --bounce true --allBalance false --payload "$message"
