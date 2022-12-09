e2c49004c65f1121e6fd2bcc8365a9f62435ee0f
Renat Gofman <renatgofman@gmail.com>
2022-12-09 10:53:11 -0800

    add supported tokens

--- everdues/contracts/root/EverduesRootStorage.sol
+++ everdues/contracts/root/EverduesRootStorage.sol
@@ -95,7 +95,7 @@ abstract contract EverduesRootStorage {
 	mapping(uint8 => mapping(uint256 => ExternalSubscription))
 		public cross_chain_subscriptions;
 
-	mapping (uint8=>string[]) supported_external_tokens;
+	mapping (uint8=>string[]) public supported_external_tokens;
 
 	function getCodeHashes(
 		uint256 owner_pubkey
